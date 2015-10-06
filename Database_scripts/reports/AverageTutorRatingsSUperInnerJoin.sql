42ce2416634ab9d9b7f8f33b4cde641bafeeba71
Jason Chalom <jasonchalom@yahoo.co.uk>
2015-10-06 21:15:24 +0200

    Forgot this

---
+++ Database_scripts/reports/AverageTutorRatingsSUperInnerJoin.sql
@@ -0,0 +1,7 @@
+SELECT        AVG(ATTENDANCE.student_activity_tutorrating) AS AverageTutorRating, TUTOR.tutor_id
+FROM            ATTENDANCE INNER JOIN
+                         STUDENT ON ATTENDANCE.student_id = STUDENT.student_id INNER JOIN
+                         [GROUP] ON STUDENT.group_id = [GROUP].group_id INNER JOIN
+                         SESSION ON [GROUP].group_id = SESSION.group_id INNER JOIN
+                         TUTOR ON SESSION.tutor_id = TUTOR.tutor_id
+GROUP BY TUTOR.tutor_id
